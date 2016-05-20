048071be807c7e6eaef7f0d07b03241b087b1e22
papapumpnz <papapumpnz@msn.com>
2016-05-21 09:04:01 +1200

    update

--- auction-server-auth/start_mongodb-repair.bat
+++ auction-server-auth/start_mongodb-repair.bat
@@ -3,4 +3,4 @@ SET data_directory="mongodb_data"
 SET working_directory=%~dp0
 
 :RUNMONGO
-E:\mongodb\bin\mongod.exe --repair --dbpath "%working_directory%/%data_directory%"
+mongod.exe --repair --dbpath "%working_directory%/%data_directory%"
